d714c9cad564f778cd5b0bb94eed9aaf0bffba14
MSI\alanc <alanchacon94@gmail.com>
2021-03-20 04:28:08 -0600

    no message

---
+++ ShoppingCart/Module1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MultiplicationOfValues(1, 2));
+        }
+        static int MultiplicationOfValues(int value1,int value2)
+        {
+            int a = 900;
+            int b = 3;
+            int result = 0;
+            result = a * b;
+
+            return result;
+        }
+    }
+}
